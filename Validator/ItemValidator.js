dac0670b4baf5a1a686fd6e186ea104e9af9c4b3
Dias <higordiaszuqueto@gmail.com>
2022-03-29 15:08:35 -0300

    update

--- Validator/ItemValidator.js
+++ Validator/ItemValidator.js
@@ -5,5 +5,25 @@
  * @function
  */
 export function ValidateItemDrop(itemDrop) {
-
+    if (!itemDrop)
+        return false;
+    if (!itemDrop.maxAmount)
+        return false;
+    if (!itemDrop.minAmount)
+        return false;
+    if (!itemDrop.rarity)
+        return false;
+    if (!itemDrop.name)
+        return false;
+    if (!Number.isSafeInteger(itemDrop.maxAmount))
+        return false;
+    if (!Number.isSafeInteger(itemDrop.minAmount))
+        return false;
+    if (!Number.isSafeInteger(itemDrop.rarity))
+        return false;
+    if (!typeof itemDrop.name == 'string')
+        return false;
+    if (itemDrop.rarity > 10 || itemDrop.rarity < 0)
+        return false;
+    return true;
 }
